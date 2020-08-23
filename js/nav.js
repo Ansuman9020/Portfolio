8ec9b077fa786b1dc53fad803a669ef7128a6808
Ansuman Samal <52194650+Ansuman9020@users.noreply.github.com>
2020-08-23 10:34:52 +0530

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+
+
+
+cont hamBurger=document.querySelector(".ham-burger");
+hamBurger.addEventListener("click",function(){
+   document.querySelector(".navbar").classList.toggle("show");
+})cont navbar=document.querySelector(".navbar");
+a=navbar.querySelectorAll("a");
+
+a.forEach(function(element){
+    element.addEventListener("click",function(){
+        for(let i=; i<a.length; i++){
+            a[i].classList.remove("active");
+        }
+        this.classList.add("active")
+    })
+})
+
+
